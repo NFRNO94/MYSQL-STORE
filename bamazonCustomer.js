f8fd21829f382a68f3ad8859e1918c56fa828c7b
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-05-04 09:08:40 -0700

    I have the table content showing in console. Still need table view

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,32 +4,27 @@ const inquirer = require("inquirer");
 
 let connection = mysql.createConnection({
   host: "localhost",
-
   // Your port; if not 3306
   port: 3306,
-
   // Your username
   user: "root",
-
   // Your password
-  password: "12069418Dp",
+  password: "",
   database: "bamazon_DB"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
 
-    console.log("connected as id " + connection.threadId + "\n");
-    
-    connection.end(function(err) {
+    //console.log("connected as id " + connection.threadId + "\n");
+    connection.query("SELECT * FROM products", function (err, result, fields) {
       if (err) throw err;
-      
-      console.log("connection ended");
-    
-    });
-
-    //createProduct();
+      console.log(result);
   });
+});
+
+
+
 
   /*function createProduct() {
     console.log("Inserting a new product...\n");
@@ -51,6 +46,12 @@ connection.connect(function(err) {
     // logs the actual query being run
     console.log(query.sql);
 
+    connection.end(function(err) {
+      if (err) throw err;
+      
+      console.log("connection ended");
+    
+    });
     
   }*/
 
