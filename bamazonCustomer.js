ad8bbd02cfc8ca54c85f6167f91d2268ac6a49eb
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-05-05 22:22:42 -0700

    introduced prompt to select item id to buy, and ask for the quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-
 let connection = mysql.createConnection({
   host: "localhost",
   // Your port; if not 3306
@@ -15,17 +14,14 @@ let connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-
     start();
-    end();
 });
-    //console.log("connected as id " + connection.threadId + "\n");
     
     function start () {
       connection.query("SELECT*FROM products", function (err, result, fields) {
         if (err) throw err;
         //console.log(result);
-      for(var i = 0; i < result.length; i++) {
+      for(let i = 0; i < result.length; i++) {
         console.log(
         "----------------------------------------------------------------------------------------------------------\n"
         + "ID: "  + result[i].item_id + " | " + "Item: " + result[i].product_name + " | " 
@@ -33,8 +29,46 @@ connection.connect(function(err) {
         + result[i].price + " | " + "Quantity: " + result[i].stock_quantity
         + "\n----------------------------------------------------------------------------------------------------------");
       }
-    });
-  }
+    
+      inquirer
+      .prompt({
+      name: "buyItem",
+      type: "input",
+      message: "What is the ID of the product you wish to buy?",
+      choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    })
+    .then(function(answer) {
+      if (answer.buyItem > 0 && answer.buyItem < 13) {
+        unitQuantity();
+      } else{
+        console.log("Re-run the code, and enter a valid item ID.")
+        end();
+      }
+    })
+  });
+}
+
+  function unitQuantity() {
+    inquirer
+    .prompt([
+      {
+        name: "quantity",
+        type: "input",
+        message: "Enter quantity of the item to be purchased."
+      },
+    ])
+    .then(function(answer) {
+  })
+  console.log("unit quantity function ran");
+  end();
+};
+
+
+      //customerPrompt();
+
+  //function customerPrompt () {
+    
+ // }
 
   function end () {
     connection.end(function(err) {
