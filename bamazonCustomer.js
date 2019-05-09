3cfa1b21e23b9b5b1cfb76c49dadd8769495c75e
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-05-08 21:03:52 -0700

    prompt now works for quantity and selecting item, then updates db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,15 +12,17 @@ let connection = mysql.createConnection({
   database: "bamazon_DB"
 });
 
+//start connection to the db
 connection.connect(function (err) {
   if (err) throw err;
   start();
 });
 
+//console log the table results from the connection
 function start() {
   connection.query("SELECT*FROM products", function (err, result, fields) {
     if (err) throw err;
-    //console.log(result);
+
     for (let i = 0; i < result.length; i++) {
       console.log(
         "----------------------------------------------------------------------------------------------------------\n"
@@ -29,15 +31,24 @@ function start() {
         + result[i].price + " | " + "Quantity: " + result[i].stock_quantity
         + "\n----------------------------------------------------------------------------------------------------------");
     }
+    //run function give prompt to buy an item
+    selectItemToBuy();
+  })
+}
+
+//select an item to buy, and select quantity
+function selectItemToBuy() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
     inquirer
       .prompt([
         {
-          name: "buyItem",
+          name: "choice",
           type: "rawlist",
           choices: function () {
-            let choiceArray = [];
-            for (let i = 0; i < result.length; i++) {
-              choiceArray.push(result[i].product_name);
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
             }
             return choiceArray;
           },
@@ -50,38 +61,63 @@ function start() {
         }
       ])
       .then(function (answer) {
-        let chosenItem;
-          if (answer.buyItem) {
-            chosenItem = answer.buyItem;
-            console.log("Chosen item: " + chosenItem);
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === answer.choice) {
+            chosenItem = results[i];
           }
-        if (parseInt(answer.quantity) < chosenItem.stock_quantity) {
+        }
+
+        if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+          //update the stock of the item selected after the user inputs a quantity to buyy
           connection.query(
             "UPDATE products SET ? WHERE ?",
             [
               {
-                stock_quantity: result[i].stock_quantity - answer.quantity
+                stock_quantity: chosenItem.stock_quantity - parseInt(answer.quantity)
               },
               {
-                id: chosenItem.result[i].item_id
+                item_id: chosenItem.item_id
               }
             ],
             function (error) {
               if (error) throw err;
               console.log("Product order has been placed!");
+              continueEnd();
             }
-          )
+          );
         } else {
           console.log("There is not enough quantity of this item in stock.")
+          continueEnd();
         }
-        //if (answer.buyItem) {
-        //unitQuantity();
-        //}*/
-        end();
-      })
-  })
+      });
+  });
 }
 
+//ask user if they want to buy another item, or exit
+function continueEnd() {
+  inquirer
+    .prompt([
+      {
+        name: "buyAnother",
+        type: "list",
+        message: "Do you want to buy another item?",
+        choices: ["Continue Shopping", "Exit"]
+      }
+    ])
+    .then(function (answer) {
+      switch (answer.buyAnother) {
+        case "Continue Shopping":
+          start();
+          break;
+        case "Exit":
+          end();
+          break;
+      }
+    });
+}
+
+//end connection
 function end() {
   connection.end(function (err) {
     if (err) throw err;
